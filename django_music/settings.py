382ff41165108fbdc8e319a6b139ea20b466dd12
ladamsperez <ladamsperezwork@gmail.com>
2021-07-18 15:59:04 -0400

    Usage of Django-Heroku

--- django_music/settings.py
+++ django_music/settings.py
@@ -128,3 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
