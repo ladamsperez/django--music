dbe9d8e48167eacc7fd9f85abb797752562fb967
ladamsperez <ladamsperezwork@gmail.com>
2021-07-18 15:10:22 -0400

    last updated admin section

---
+++ LEbeats/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LEbeatsConfig(AppConfig):
+    name = 'LEbeats'
---
+++ LEbeats/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    year = models.DateTimeField(blank=True, null=True)
+    date_added = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
