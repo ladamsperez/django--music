756f6a4299fc40b49c1b8b08eb55e20b0479006b
ladamsperez <ladamsperezwork@gmail.com>
2021-07-18 15:36:17 -0400

    disable static for rn (heroku)

--- django_music/settings.py
+++ django_music/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-by@seithj5yf0-m^5s&x5&zg49n+gf$wq51=0wdnmzjn9q*9_^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
